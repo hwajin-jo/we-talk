b73878f75f3fc6c275794d47b9f1a193f139bb9d
hwajin-jo <wa8784@naver.com>
2025-12-08 16:18:09 +0900

    first commit

---
+++ src/main/java/com/websocket/wetalk/WetalkApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.wetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetalkApplication.class, args);
+	}
+
+}
